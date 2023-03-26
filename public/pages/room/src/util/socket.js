f71d49daa1565c5f59793ecb856fc6975b11a93a
GugaSantos <guga.santos.guga@gmail.com>
2023-03-26 09:00:06 -0300

    feat: create build method at SocketBuilder class

--- public/pages/room/src/util/socket.js
+++ public/pages/room/src/util/socket.js
@@ -2,4 +2,13 @@ class SocketBuilder {
   constructor({ socketUrl }) {
     this.socketUrl = socketUrl;
   }
+
+  build() {
+    const socket = io.connect(this.socketUrl, {
+      withCredentials: false
+    })
+
+    socket.on('user-connected');
+    socket.on('user-disconnected');
+  }
 }
